e804d70f96509be2609b0dda3daefc5610a9121c
Eugene <Eugene_Zaukin@mail.ru>
2021-04-20 17:38:03 +0700

    Tasks Вроде все получилось, только у меня картинки накладывались друг на друга которые определены в MainFragment c 66 по 70 строки. из за чего так происходит? imageView?.load(url) {                         lifecycle(this@MainFragment)                         error(R.drawable.errorimage)                         placeholder(R.drawable.formatimage)                     }

--- app/src/main/java/com/eugene/androidmaterialdesign/ui/main/MainViewModel.kt
+++ app/src/main/java/com/eugene/androidmaterialdesign/ui/main/MainViewModel.kt
@@ -1,7 +1,54 @@
 package com.eugene.androidmaterialdesign.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.eugene.androidmaterialdesign.BuildConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+class MainViewModel (
+    private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayData> = MutableLiveData(),
+    private val retrofitImpl: PODRetrofitImpl = PODRetrofitImpl()
+) : ViewModel() {
+
+    fun getData(): LiveData<PictureOfTheDayData> {
+        sendServerRequest()
+        return liveDataForViewToObserve
+    }
+
+    private fun sendServerRequest() {
+        liveDataForViewToObserve.value = PictureOfTheDayData.Loading(null)
+        val apiKey: String = BuildConfig.NASA_API_KEY
+
+        if (apiKey.isBlank()) {
+            PictureOfTheDayData.Error(Throwable("You need API key"))
+        } else {
+            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(object : Callback<PODServerResponseData> {
+                override fun onResponse(
+                    call: Call<PODServerResponseData>,
+                    response: Response<PODServerResponseData>
+                ) {
+                    if (response.isSuccessful && response.body() != null) {
+                        liveDataForViewToObserve.value =
+                            PictureOfTheDayData.Success(response.body()!!)
+                    } else {
+                        val message = response.message()
+                        if (message.isNullOrEmpty()) {
+                            liveDataForViewToObserve.value = PictureOfTheDayData.Error(Throwable("Unidentified error"))
+                        } else {
+                            liveDataForViewToObserve.value = PictureOfTheDayData.Error(Throwable(message))
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<PODServerResponseData>, t:
+                Throwable) {
+                    liveDataForViewToObserve.value = PictureOfTheDayData.Error(t)
+                }
+            })
+        }
+    }
 }

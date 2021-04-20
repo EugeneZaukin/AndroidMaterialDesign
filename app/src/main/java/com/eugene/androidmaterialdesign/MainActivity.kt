687f43688e2d83b7c72d68ab41435b507316fca2
Eugene <Eugene_Zaukin@mail.ru>
2021-04-20 11:06:52 +0700

    Initial commit

---
+++ app/src/main/java/com/eugene/androidmaterialdesign/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.eugene.androidmaterialdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eugene.androidmaterialdesign.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/eugene/androidmaterialdesign/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.eugene.androidmaterialdesign.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

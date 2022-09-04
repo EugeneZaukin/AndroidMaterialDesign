2d9e2aa17f601f45a55e793b7c52299386cdb62d
Eugene_Zaukin <ezapach@yandex.ru>
2022-09-04 11:48:00 +0300

    Fix splash activity

--- app/src/main/java/com/eugene/androidmaterialdesign/SplashActivity.kt
+++ app/src/main/java/com/eugene/androidmaterialdesign/SplashActivity.kt
@@ -4,31 +4,33 @@ import android.animation.Animator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.LayoutInflater
 import android.view.animation.LinearInterpolator
-import kotlinx.android.synthetic.main.activity_splash.*
+import com.eugene.androidmaterialdesign.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+private const val ROTATION_BY_Y = 300f
+private const val DURATION_ANIMATION = 3000L
+
+class SplashActivity : AppCompatActivity(), Animator.AnimatorListener {
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
-        splash_image_view.animate().rotationYBy(300f)
+        binding.splashImageView.animate().rotationYBy(ROTATION_BY_Y)
             .setInterpolator(LinearInterpolator())
-            .setDuration(3000)
-            .setListener(object : Animator.AnimatorListener {
-
-                override fun onAnimationEnd(animation: Animator?) {
-                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                    finish()
-                }
-
-                override fun onAnimationStart(animation: Animator?) {}
-                override fun onAnimationCancel(animation: Animator?) {}
-                override fun onAnimationRepeat(animation: Animator?) {}
-
-            })
+            .setDuration(DURATION_ANIMATION)
+            .setListener(this)
     }
+
+    override fun onAnimationEnd(p0: Animator?) {
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
+    }
+
+    override fun onAnimationStart(p0: Animator?) {}
+    override fun onAnimationCancel(p0: Animator?) {}
+    override fun onAnimationRepeat(p0: Animator?) {}
 }
